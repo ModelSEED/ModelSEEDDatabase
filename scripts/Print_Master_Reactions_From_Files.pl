801a704c79f2f3037784459cba6546f6017bde1c
seaver <seaver@mcs.anl.gov>
2015-08-27 14:28:27 -0500

    Update to printing master reactions directly from file

--- scripts/Print_Master_Reactions_From_Files.pl
+++ scripts/Print_Master_Reactions_From_Files.pl
@@ -20,15 +20,6 @@ my @temp=();
 #Initialization
 #######################################################
 
-# The methods for building equations in a Reaction object can only be used if
-# there is a Biochemistry object with all of the valid compounds.  So first
-# create a new Biochemistry object and add all of the compounds and compartments
-# before processing reactions.
-
-# Create a new Biochemistry object (starting from scratch).  Set the reference so it
-# is not in the default UUID format.
-my $biochem = Bio::KBase::ObjectAPI::KBaseBiochem::Biochemistry->new({"id" => "throw_away", "_reference" => "~"});
-
 # Get the master list of compounds from the source file.
 # Note that the master list has already had modifications applied.
 my %Compounds=();
@@ -39,34 +30,29 @@ while(<FH>){
     chomp;
     @temp=split(/\t/,$_,-1);
     for (my $i=0;$i<scalar(@temp);$i++) { # Add all of the fields
-		$Compounds{$temp[0]}{$headers[$i]} = $temp[$i];
+	$Compounds{$temp[0]}{$headers[$i]} = $temp[$i];
     }
+    
+    # Remove unknown values from hash so compound object defaults are used instead.
+    if ($Compounds{$temp[0]}{"charge"} eq "null") {
+	delete $Compounds{$temp[0]}{"charge"};
+    }
+    if ($Compounds{$temp[0]}{"mass"} eq "null") {
+	delete $Compounds{$temp[0]}{"mass"};
+    }
+    if ($Compounds{$temp[0]}{"deltag"} eq "null") {
+	delete $Compounds{$temp[0]}{"deltag"};
+    }
+    if ($Compounds{$temp[0]}{"deltagerr"} eq "null") {
+	delete $Compounds{$temp[0]}{"deltagerr"};
+    }
+    
+    # Name needs to be specified as an array.
+    $Compounds{$temp[0]}{"names"} = [ $Compounds{$temp[0]}{"name"} ];
+    delete $Compounds{$temp[0]}{"structure"}; # do I need to build a CompoundStructure object here?
+
 }
 close(FH);
-
-# Add each compound to the biochemistry object.
-foreach my $cpd (sort keys %Compounds) { # grep { $_ ne "cpd00000" }
-	# Remove unknown values from hash so compound object defaults are used instead.
-	if ($Compounds{$cpd}{"charge"} eq "null") {
-		delete $Compounds{$cpd}{"charge"};
-	}
-	if ($Compounds{$cpd}{"mass"} eq "null") {
-		delete $Compounds{$cpd}{"mass"};
-	}
-	if ($Compounds{$cpd}{"deltag"} eq "null") {
-		delete $Compounds{$cpd}{"deltag"};
-	}
-	if ($Compounds{$cpd}{"deltagerr"} eq "null") {
-		delete $Compounds{$cpd}{"deltagerr"};
-	}
-	
-	# Name needs to be specified as an array.
-	$Compounds{$cpd}{"names"} = [ $Compounds{$cpd}{"name"} ];
-	delete $Compounds{$cpd}{"structure"}; # do I need to build a CompoundStructure object here?
-	
-	# Add the compound.
-	$biochem->addCompoundFromHash($Compounds{$cpd});
-}
 
 # Get the master list of compartments from the source file.
 my %Compartments=();
@@ -77,114 +63,169 @@ while(<FH>){
     chomp;
     @temp=split(/\t/,$_,-1);
     for (my $i=0;$i<scalar(@temp);$i++) { # Add all of the fields
-		$Compartments{$temp[0]}{$headers[$i]} = $temp[$i];
+	$Compartments{$temp[0]}{$headers[$i]} = $temp[$i];
     }
 }
 close(FH);
-
-# Add each compartment to the biochemistry object.
-foreach my $cpt (sort keys %Compartments) {
-	# Add the compartment.
-	$biochem->addCompartmentFromHash($Compartments{$cpt});
-}
 
 # Load up the required modifications for reactions.
 open(FH, "< ".$opt->mods);
 my %Rxn_Mods=();
 while(<FH>){
-	chomp;
-	@temp = split(/\t/,$_,-1);
-	$Rxn_Mods{$temp[0]}{$temp[2]}=$temp[3];
-	if($temp[2] eq "replace"){
-		$Rxn_Mods{$temp[0]}{$temp[2]} = [$temp[3], $temp[4]];
-	}
+    chomp;
+    @temp = split(/\t/,$_,-1);
+    $Rxn_Mods{$temp[0]}{$temp[2]}=$temp[3];
+    if($temp[2] eq "replace"){
+	$Rxn_Mods{$temp[0]}{$temp[2]} = [$temp[3], $temp[4]];
+    }
 }
 close(FH);
 
 my %Rxns_Codes=();
 my %Codes_Rxns=();
-my %Reactions=(); # Master list of reactions
+my %Reactions=(); #Master list of reactions
 
 # Process each of the source files.  Note that if a reaction is defined in multiple
 # source files, the reaction from the last file processed is used.
 foreach my $db ("default", "plantdefault") {
-	open(FH, "< ../Biochemistry/reactions.".$db.".tsv");
-	@headers = split(/\t/,<FH>);
-	chomp $headers[$#headers];
-	while(<FH>){
-	    chomp;
-	    @temp=split(/\t/,$_,-1);
+    
+    # The methods for building equations in a Reaction object can only be used if
+    # there is a Biochemistry object with all of the valid compounds.  So first
+    # create a new Biochemistry object and add all of the compounds and compartments
+    # before processing reactions.
+
+    # Create a new Biochemistry object (starting from scratch).  Set the reference so it
+    # is not in the default UUID format.
+    my $biochem = Bio::KBase::ObjectAPI::KBaseBiochem::Biochemistry->new({"id" => $db."_throw_away", "_reference" => "~"});
+
+    # Add each compartment to the biochemistry object.
+    foreach my $cpt (sort keys %Compartments) {
+	# Add the compartment.
+	$biochem->addCompartmentFromHash($Compartments{$cpt});
+    }
+
+    # Add each compartment to the biochemistry object.
+    foreach my $cpd (sort keys %Compounds) {
+	# Add the compound
+	$biochem->addCompoundFromHash($Compounds{$cpd});
+    }
+
+    open(FH, "< ../Biochemistry/reactions.".$db.".tsv");
+    @headers = split(/\t/,<FH>);
+    chomp $headers[$#headers];
+    while(<FH>){
+	chomp;
+	@temp=split(/\t/,$_,-1);
 	
-		# Assume that the id is in field 0.
-		my $rxnid = $temp[0];
-		my %rxnhash=();
-		# Skip the reaction when the modification is to give priority to different database.
-		if (exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{priority}) && $Rxn_Mods{$rxnid}{priority} ne $db) {
-			next;
-		}
+	# Assume that the id is in field 0.
+	my $rxnid = $temp[0];
+
+	#skip removed reactions
+	next if exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{remove});
 	
-		for (my $i=1;$i<scalar(@temp);$i++) {
-			if (exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{$headers[$i]})) {
-				# Apply a modification to a field.
-				$temp[$i] = $Rxn_Mods{$rxnid}{$headers[$i]};
-			}
-	
-			# Add this field to the reaction in the master list.
-			$Reactions{$rxnid}{$headers[$i]} = $temp[$i];
-			$rxnhash{$rxnid}{$headers[$i]} = $temp[$i]; # Copy for adding to biochemistry object
-		}
-	
-		# Remove unknown values so reaction object defaults are used instead.
-		if ($rxnhash{$rxnid}{"deltag"} eq "null") {
-			delete $rxnhash{$rxnid}{"deltag"};
-		}
-		if ($rxnhash{$rxnid}{"deltagerr"} eq "null") {
-			delete $rxnhash{$rxnid}{"deltagerr"};
-		}
-	
-		# Name needs to be specified as an array.
-		$rxnhash{$rxnid}{"names"} = [ $rxnhash{$rxnid}{"name"} ];
-		
-		# Add the reaction.
-		$rxnhash{$rxnid}{"id"} = $rxnid;
-		my $rxn = $biochem->addReactionFromHash($rxnhash{$rxnid});
-	
-		# Apply modification to replace a compound.
-		if (exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{replace})) {
-			# Make sure replacement compound exists.
-			my $repCpd = $biochem->getObject("compounds", $Rxn_Mods{$rxnid}{replace}[1]);
-			if (!$repCpd) {
-				print "Replacement compound ".$Compounds{$Rxn_Mods{$rxnid}{replace}[1]}." does not exist\n";
-				next;
-			}
-	
-			# Find the compound in the list of reagents and replace it.
-			my $foorxn = $biochem->searchForReaction($rxnid);
-			foreach my $rgt (@{$foorxn->reagents()}) {
-				if ($rgt->compound()->id() eq $Rxn_Mods{$rxnid}{replace}[0]) {
-					$rgt->compound($repCpd);
-				}
-			}
-		}
-	
-		# Generate a md5 of the equation for merging later.
-		my $code = $rxn->genEquationCode();
-		if (!exists($Codes_Rxns{$code})) { # If not unique, generate md5 with reversed equation.
-			$code = $rxn->revGenEquationCode();
-		}
-	
-		# Map reaction IDs to md5.
-		$Codes_Rxns{$code}{$rxnid}=1;
-		$Rxns_Codes{$rxnid}{$code}=1;
-		
-		# Update fields that use compounds.
-		$Reactions{$rxnid}{equation} = $rxn->genEquation();
-		$Reactions{$rxnid}{code} = $rxn->genCode();
-		$Reactions{$rxnid}{definition} = $rxn->genDefinition();
-		$Reactions{$rxnid}{stoichiometry} = $rxn->genStoichiometry();
-		
+	# Skip the reaction when the modification is to give priority to different database.
+	if (exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{priority}) && $Rxn_Mods{$rxnid}{priority} ne $db) {
+	    next;
 	}
-	close(FH);
+	
+	#skip the reaction if already retrieved from one database
+	#this means that unless otherwise stated in the priority
+	#we default to reactions from the default database
+	next if exists($Reactions{$rxnid});
+	
+	my %rxnhash=(id=>$rxnid);
+	for (my $i=1;$i<scalar(@temp);$i++) {
+	    if (exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{$headers[$i]})) {
+		# Apply a modification to a field.
+		$temp[$i] = $Rxn_Mods{$rxnid}{$headers[$i]};
+	    }
+	    $rxnhash{$headers[$i]} = $temp[$i]; # Copy for adding to biochemistry object
+	}
+	
+	# Remove unknown values so reaction object defaults are used instead.
+	if ($rxnhash{"deltag"} eq "null") {
+	    delete $rxnhash{"deltag"};
+	}
+	if ($rxnhash{"deltagerr"} eq "null") {
+	    delete $rxnhash{"deltagerr"};
+	}
+	
+	# Name needs to be specified as an array.
+	$rxnhash{"names"} = [ $rxnhash{"name"} ];
+	
+	#update any modifications
+	if (exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{replace})) {
+	    my ($old_cpd,$new_cpd) = @{$Rxn_Mods{$rxnid}{replace}};
+	    
+	    # Make sure replacement compound exists.
+	    my $repCpd = $biochem->getObject("compounds", $old_cpd);
+	    if (!$repCpd) {
+		print "Replacement compound ".$old_cpd." does not exist\n";
+		next;
+	    }
+
+	    $rxnhash{equation} =~ s/${old_cpd}/${new_cpd}/g;
+	    $rxnhash{code} =~ s/${old_cpd}/${new_cpd}/g;
+	    $rxnhash{definition} =~ s/${old_cpd}/${new_cpd}/g;
+	    $rxnhash{stoichiometry} =~ s/${old_cpd}/${new_cpd}/g;
+	}
+
+	$rxnhash{findmatch}=0;
+	my $rxn = $biochem->addReactionFromHash(\%rxnhash);
+	$rxn->status($rxnhash{status});
+	$rxn->checkReactionMassChargeBalance({rebalanceProtons=>1,rebalanceWater=>0,saveStatus=>1});
+	
+	# Generate a md5 of the equation for merging later.
+	my $code = $rxn->genEquationCode();
+	if (!exists($Codes_Rxns{$code})) { # If not unique, generate md5 with reversed equation.
+	    $code = $rxn->revGenEquationCode();
+	}
+	
+	# Map reaction IDs to md5.
+	$Codes_Rxns{$code}{$rxnid}=1;
+	$Rxns_Codes{$rxnid}{$code}=1;
+	
+	$Reactions{$rxnid}{equation} = $rxn->genEquation();
+	$Reactions{$rxnid}{code} = $rxn->genCode();
+	$Reactions{$rxnid}{definition} = $rxn->genDefinition();
+	$Reactions{$rxnid}{stoichiometry} = $rxn->genStoichiometry();
+	$Reactions{$rxnid}{status} = $rxn->status();
+	
+    }
+    close(FH);
+}
+
+foreach my $db ("default", "plantdefault") {
+    open(FH, "< ../Biochemistry/reactions.".$db.".tsv");
+    @headers = split(/\t/,<FH>);
+    chomp $headers[$#headers];
+    while(<FH>){
+	chomp;
+	@temp=split(/\t/,$_,-1);
+	
+	# Assume that the id is in field 0.
+	my $rxnid = $temp[0];
+	
+	#skip removed reactions
+	next if exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{remove});
+
+	# Skip the reaction when the modification is to give priority to different database.
+	if (exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{priority}) && $Rxn_Mods{$rxnid}{priority} ne $db) {
+	    next;
+	}
+	
+	for (my $i=1;$i<scalar(@temp);$i++) {
+	    if (exists($Rxn_Mods{$rxnid}) && exists($Rxn_Mods{$rxnid}{$headers[$i]})) {
+		# Apply a modification to a field.
+		$temp[$i] = $Rxn_Mods{$rxnid}{$headers[$i]};
+	    }
+
+	    if($headers[$i] !~ /status|equation|definition|code|stoichiometry/){
+		$Reactions{$rxnid}{$headers[$i]} = $temp[$i];
+	    }
+
+	}
+    }
 }
 
 #Print it all out
