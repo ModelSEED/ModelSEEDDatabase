8225b9ca0dbfcfbcfc8b4e5c9bfc39f1995e7810
Mike Mundy <mundy.michael@mayo.edu>
2015-10-13 13:25:53 -0500

    add option to show enzymes that are skipped

--- scripts/Print_KEGG_Complex_Role.py
+++ scripts/Print_KEGG_Complex_Role.py
@@ -6,7 +6,7 @@ from biokbase.probabilistic_annotation.kegg.KEGGEnzymeDatabase import KEGGEnzyme
 
 desc1 = '''
 NAME
-      Print_KEGG_Complex_Role -- 
+      Print_KEGG_Complex_Role -- build complex and role files from KEGG enzyme database
 
 SYNOPSIS
 '''
@@ -19,7 +19,7 @@ desc3 = '''
 EXAMPLES
       
 SEE ALSO
-      Print_Complex_Roles.pl
+      Print_KEGG_Complex_Roles.py --directory ../Mappings
 
 AUTHORS
       Mike Mundy 
@@ -30,7 +30,8 @@ def nameToSearchName(name):
     searchName = name.lower()
     searchName = re.sub(r'[\d\-]+\.[\d\-]+\.[\d\-]+\.[\d\-]+', r'', searchName) # Remove EC number
     searchName = re.sub(r'\s', r'', searchName) # Remove whitespace
-    searchName = re.sub(r'\#.*$', r'', searchName) # Remove special characters
+    searchName = re.sub(r'\#.*$', r'', searchName) # Remove comments from end
+    searchName = re.sub(r'\(ec\)', '', searchName) # Remove EC number prefix
     return searchName
 
 if __name__ == "__main__":
@@ -38,6 +39,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, prog='Print_KEGG_Complex_Role', epilog=desc3)
     parser.add_argument('--enzymedb', help='path to file with KEGG enzyme database', action='store', dest='enzymeDB', default='enzyme.db')
     parser.add_argument('--directory', help='path to directory for storing complex role file', action='store', dest='directory', default='.')
+    parser.add_argument('--show-skipped', help='show enzymes that were skipped and stored in complex role file', action='store_true', dest='showSkipped', default=False)
     usage = parser.format_usage()
     parser.description = desc1 + '      ' + usage + desc2
     parser.usage = argparse.SUPPRESS
@@ -47,7 +49,9 @@ if __name__ == "__main__":
     enzymeDB = KEGGEnzymeDatabase(args.enzymeDB)
     enzymeDB.load()
     
-    # Assign a complex ID to every enzyme in the enzyme database.
+    # Assign a complex ID to every enzyme in the enzyme database. Note that if enzymes
+    # are added to the database, enzymes will be assigned different complex IDs when
+    # this script is run again.
     nextComplexId = 10000
     enzymeComplexDict = dict()
     for key in sorted(enzymeDB.enzymes):
@@ -57,14 +61,19 @@ if __name__ == "__main__":
     # Build a dictionary keyed by complex ID with a list of roles.
     # We call the enzyme the complex and the roles are the names of the enzyme.
     complexToRoles = dict()
+    skippedComplex = dict()
     for key in enzymeComplexDict:
         enzyme = enzymeDB.get(key)
         if len(enzyme.name) > 0 and not enzyme.name[0].startswith('Transferred to') and not enzyme.name[0].startswith('Deleted entry'):
             complexToRoles[enzymeComplexDict[key]] = list()
             for nindex in range(len(enzyme.name)):
                 complexToRoles[enzymeComplexDict[key]].append('%s (EC %s)' %(enzyme.name[nindex], key))
-#         else:
-#             complexToRoles[enzymeComplexDict[key]].append('Unknown (EC %s)' %(key))
+        else:
+            if len(enzyme.name) > 0:
+                name = enzyme.name[0]
+            else:
+                name = '(not available)'
+            skippedComplex[key] = ': assigned ID '+enzymeComplexDict[key]+' with name '+name
 
     # Build the list of field names in the complex role file.
     fieldNames = [ 
@@ -108,3 +117,10 @@ if __name__ == "__main__":
             data.append('1')
             data.append('0')
             handle.write('\t'.join(data)+'\n')
+    
+    # Show details on the enzymes that were skipped and not stored in the complex role file.        
+    if args.showSkipped:
+        for key in skippedComplex:
+            print key+skippedComplex[key]
+
+    exit(0)
