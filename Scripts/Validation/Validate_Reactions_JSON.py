2bdd77bdc34351a714a5d9a3587ff38fd4026967
JamesJeffryes <jamesgjeffryes@gmail.com>
2017-09-21 16:12:34 -0500

    json rxn checking

---
+++ Scripts/Validation/Validate_Reactions_JSON.py
@@ -0,0 +1,72 @@
+import Scripts.Validation.Schemas as schemas
+from Scripts.Validation.error_reporting import find_new_errors, report_errors, get_master_errors
+from repostat.stash import StatStash
+from jsonschema import Draft4Validator
+from collections import defaultdict, Counter
+import re
+import argparse
+import json
+
+
+def validate_schema(_rxns, verbose):
+    v = Draft4Validator(schemas.reactions)
+    schema_errors = sorted(v.iter_errors(rxns), key=lambda x: x.path)
+    if verbose:
+        for e in schema_errors:
+            print("Schema Validation ERROR at reaction[{}]: {}"
+                  .format("][".join(e.absolute_path), e.message))
+    return len(schema_errors)
+
+
+def check_dups(_rxns, verbose, unique_fields=('id', 'abbreviation',
+                                              'name', 'equation')):
+    unique_values = dict([(x, defaultdict(list)) for x in unique_fields])
+    for id, comp in _rxns.items():
+        if comp['is_obsolete'] == "1":
+            continue
+        for key in unique_values:
+            unique_values[key][comp[key]].append(id)
+
+    duplicated = defaultdict(int)
+    for value_type in unique_values:
+        for key, ids in unique_values[value_type].items():
+            if len(ids) > 1 and key != 'null':
+                if verbose:
+                    print("Duplicate {}: {} in {}".format(value_type, key, ids))
+                duplicated["duplicated_"+value_type] += len(ids)-1
+    return duplicated
+
+
+def check_compounds(_rxns, verbose, compound_loc='./Biochemistry/compounds.json'):
+    err = {'obsolete_comps': 0}
+    compounds = json.load(open(compound_loc))
+    obsolete_comps = set(cid for cid, comp in compounds.items()
+                         if comp['is_obsolete'] == '1')
+    for id, rxn in _rxns.items():
+        if rxn['is_obsolete'] == "1":
+            continue
+        comp_ids = set(rxn['compound_ids'].split(';'))
+        if comp_ids & obsolete_comps:
+            if verbose:
+                print('Obsolete compounds in {}: {}'
+                      .format(id, comp_ids & obsolete_comps))
+            err['obsolete_comps'] += 1
+    return err
+
+
+if __name__ == "__main__":
+    # Parse options.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rxnfile', help='path to reactions file', action='store')
+    parser.add_argument('-d', '--print-duplicates', help='Print all duplicated values', action='store_true', dest='print_dups', default=False)
+    parser.add_argument('-s', '--print-schema', help='Print all schema violations', action='store_true', dest='print_schema', default=False)
+    parser.add_argument('-c', '--print-compound', help='Print all compound related violations', action='store_true', dest='print_compound', default=False)
+    args = parser.parse_args()
+    rxns = json.load(open(args.rxnfile))
+    errors = {}
+    errors['schema_violations'] = validate_schema(rxns, args.print_schema)
+    errors.update(check_dups(rxns, args.print_dups))
+    errors.update(check_compounds(rxns, args.print_compound))
+    print("\nError counts")
+    for error_type, count in errors.items():
+        print("\t{}: {}".format(error_type, count))
