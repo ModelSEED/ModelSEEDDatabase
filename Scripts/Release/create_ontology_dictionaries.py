44fea05cac4ce05acc2c4b169cf4c8b708550dbc
Jeff Kimbrel <jakpot@gmail.com>
2018-11-30 11:45:58 -0600

    Add files via upload

--- Scripts/Release/create_ontology_dictionaries.py
+++ Scripts/Release/create_ontology_dictionaries.py
@@ -3,6 +3,7 @@ import os
 import json
 import datetime
 import argparse
+from Bio.ExPASy import Enzyme
 
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -62,7 +63,7 @@ def get_KEGG_KOs():
 
             kegg_dict['term_hash'][id] = {'id' : id, 'name' : name, 'synonyms' : synonym.split(", ")}
 
-    with open('KEGG_KO_ontologyDictrionary.json', 'w') as outfile:
+    with open('KEGG_KO_ontologyDictionary.json', 'w') as outfile:
         json.dump(kegg_dict, outfile, indent = 2)
 
 def get_KEGG_RXNs():
@@ -102,11 +103,50 @@ def get_KEGG_RXNs():
 
             kegg_dict['term_hash'][id] = {'id' : id, 'name' : name, 'synonyms' : fullLineSplit}
 
-    with open('KEGG_RXN_ontologyDictrionary.json', 'w') as outfile:
+    with open('KEGG_RXN_ontologyDictionary.json', 'w') as outfile:
         json.dump(kegg_dict, outfile, indent = 2)
 
+def get_EC_RXNs():
+    ec_dict = {}
+
+    # version
+    ebi_ec_release = ""
+
+    if args.test == False:
+        ebi_ec_call = 'curl ftp://ftp.ebi.ac.uk/pub/databases/enzyme/enzclass.txt'
+        ebi_ec_release = os.popen(ebi_ec_call).read()
+    else:
+        with open('enzclass.txt', 'r') as myfile:
+            ebi_ec_release = myfile.read()
+
+    ebi_ec_release = ebi_ec_release.split("\n")[7].split()[1]
+
+    ec_dict = {'data_version'    : ebi_ec_release,
+                'date'           : timestamp,
+                'format_version' : 'N/A',
+                'ontology'       : 'ec_orthology'
+                }
+
+    # parser
+
+    ebi_ec_enzyme = 'enzyme.dat'
+
+    if args.test == False:
+        ebi_ec_call = 'curl ftp://ftp.ebi.ac.uk/pub/databases/enzyme/enzyme.dat > enzyme.dat'
+        os.system(ebi_ec_call)
+
+    records = Enzyme.parse(open(ebi_ec_enzyme))
+    for record in records:
+        ec_dict[record['ID']] = {'id'       : record['ID'],
+                                 'name'     : record['DE'],
+                                 'synonyms' : record['AN']
+                                 }
+
+    with open('EBI_EC_ontologyDictionary.json', 'w') as outfile:
+        json.dump(ec_dict, outfile, indent = 2)
 
 ## RUN FUNCTIONS ###############################################################
 
 get_KEGG_KOs()
 get_KEGG_RXNs()
+get_EC_RXNs()
