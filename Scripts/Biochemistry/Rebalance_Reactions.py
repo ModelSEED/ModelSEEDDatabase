4efce255b690aec06d901b7b22ad344cc70d2709
seaver <seaver@mcs.anl.gov>
2018-12-18 13:08:16 -0600

    Last update of reaction statuses

--- Scripts/Biochemistry/Rebalance_Reactions.py
+++ Scripts/Biochemistry/Rebalance_Reactions.py
@@ -24,10 +24,10 @@ for rxn in sorted(Reactions_Dict.keys()):
 
     old_status=Reactions_Dict[rxn]["status"]
     if(new_status != old_status and "CK" not in old_status):
-        if("OK" not in old_status):
-            print rxn+"\t"+old_status+"\t"+new_status
-            Reactions_Dict[rxn]["status"]=new_status
-            Update_Reactions+=1
+#        if("OK" not in old_status):
+        print rxn+"\t"+old_status+"\t"+new_status
+        Reactions_Dict[rxn]["status"]=new_status
+        Update_Reactions+=1
 
 if(Update_Reactions>0):
     print "Saving updated statuses for "+str(Update_Reactions)+" reactions";
