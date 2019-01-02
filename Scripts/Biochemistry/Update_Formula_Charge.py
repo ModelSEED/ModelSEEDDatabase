2fcd7629acef2ff1bb89e196f896d903454e38a6
seaver <seaver@mcs.anl.gov>
2019-01-02 15:17:07 -0600

    Updating formulas and charges from latest structures

--- Scripts/Biochemistry/Update_Formula_Charge.py
+++ Scripts/Biochemistry/Update_Formula_Charge.py
@@ -17,6 +17,8 @@ Compounds_Dict = CompoundsHelper.loadCompounds()
 Structures_Dict = CompoundsHelper.loadStructures(["InChI","SMILE"],["ModelSEED"])
 
 Update_Compounds=0
+Update_Formula=0
+Update_Charge=0
 for cpd in sorted(Compounds_Dict.keys()):
     if(cpd not in Structures_Dict):
         continue
@@ -69,23 +71,25 @@ for cpd in sorted(Compounds_Dict.keys()):
             new_formula = new_formula.replace(match.group(),'')
 
     #normalizing formula my own way, so I can be consistent
-    #these are hill-sorted
+    #these are hill-sorted, and merges molecular fragments
     new_formula = Compounds.mergeFormula(new_formula)[0]
 
     current_formula = Compounds_Dict[cpd]['formula']
     if(new_formula != current_formula):
         print("Updating formula for "+cpd+" with "+new_formula)
         Compounds_Dict[cpd]['formula']=new_formula
-        Update_Compounds+=1
+        Update_Formula+=1
+        Update_Compounds=1
         
     current_charge = Compounds_Dict[cpd]['charge']
     if(str(new_charge) != current_charge):
         print("Updating charge for "+cpd+" with "+str(new_charge))
         Compounds_Dict[cpd]['charge']=str(new_charge)
-        Update_Compounds+=1
+        Update_Charge+=1
+        Update_Compounds=1
 
 if(Update_Compounds>0):
-    print("Updating "+str(Update_Compounds)+" compounds")
+    print("Updating "+str(Update_Formula)+" compounds with formula and "+str(Update_Charge)+" compounds with charge")
     CompoundsHelper.saveCompounds(Compounds_Dict)
 
 #(inchi_formula,inchi_layers) = InChIs.parse(Structures_Dict[cpd]['InChI'])
