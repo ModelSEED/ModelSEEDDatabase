300520aa229f111ba4ac69e1c0ef866d0fc77a6f
seaver <seaver@mcs.anl.gov>
2019-02-06 11:32:20 -0600

    Rewrite of listing ModelSEED structures and finding formula conflicts

--- Scripts/Structures/List_ModelSEED_Structures.py
+++ Scripts/Structures/List_ModelSEED_Structures.py
@@ -11,28 +11,49 @@ DifferObj=Differ()
 CompoundsHelper = Compounds()
 Compounds_Dict = CompoundsHelper.loadCompounds()
 
+Structures_Root="../../Biochemistry/Structures/"
+Formulas_Dict=dict()
+for source in "KEGG","MetaCyc":
+    if(source not in Formulas_Dict):
+        Formulas_Dict[source]=dict()
+
+    for struct_type in "InChI","SMILE":
+        if(struct_type not in Formulas_Dict[source]):
+            Formulas_Dict[source][struct_type]=dict()
+
+        for struct_stage in "Charged","Original":
+            if(struct_stage not in Formulas_Dict[source][struct_type]):
+                Formulas_Dict[source][struct_type][struct_stage]=dict()
+
+            file_name=Structures_Root+source+"/"+struct_type+"_"+struct_stage+"_Formulas_Charges.txt"
+            with open(file_name) as file_handle:
+                for line in file_handle.readlines():
+                    line=line.strip()
+                    array=line.split('\t')
+                    Formulas_Dict[source][struct_type][struct_stage][array[0]]=array[1]+array[2]
+
 #Load Curated Structures
 Ignored_Structures=dict()
-with open("../../Biochemistry/Structures/Curation/Ignore_Structures.txt") as ignore_file:
+with open(Structures_Root+"Curation/Ignore_Structures.txt") as ignore_file:
     for line in ignore_file.readlines():
         array=line.split('\t')
-        Ignored_Structures[array[0]]=1
+#        Ignored_Structures[array[0]]=1
 ignore_file.close()
 
 #Load Structures and Aliases
 Structures_Dict = CompoundsHelper.loadStructures(["SMILE","InChIKey","InChI"],["KEGG","MetaCyc"])
 MS_Aliases_Dict =  CompoundsHelper.loadMSAliases(["KEGG","MetaCyc"])
 
-
-unique_structs_file = open("../../Biochemistry/Structures/Unique_ModelSEED_Structures.txt",'w')
 master_structs_file = open("../../Biochemistry/Structures/All_ModelSEED_Structures.txt",'w')
-inchi_conflicts_file = open("InChI_Conflicts.txt",'w')
-smile_conflicts_file = open("SMILE_Conflicts.txt",'w')
+unique_structs_file = open("../../Biochemistry/Structures/Unique_ModelSEED_Structures.txt",'w')
 unique_structs_file.write("ID\tType\tAliases\tStructure\n")
+structure_conflicts_file = open("Structure_Conflicts.txt",'w')
+formula_conflicts_file = open("Formula_Conflicts.txt",'w')
 for msid in sorted(MS_Aliases_Dict.keys()):
 
     #Build collection of all structures for the ModelSEED ID
     Structs = dict()
+    Formulas=dict()
     for source in 'KEGG','MetaCyc':
         if(source not in MS_Aliases_Dict[msid].keys()):
             continue
@@ -45,9 +66,11 @@ for msid in sorted(MS_Aliases_Dict.keys()):
                 for struct_stage in sorted(Structures_Dict[struct_type][external_id].keys()):
                     if(struct_type not in Structs):
                         Structs[struct_type]=dict()
+                        Formulas[struct_type]=dict()
 
                     if(struct_stage not in Structs[struct_type]):
                         Structs[struct_type][struct_stage]=dict()
+                        Formulas[struct_type][struct_stage]=dict()
 
                     for structure in sorted(Structures_Dict[struct_type][external_id][struct_stage].keys()):
                             
@@ -59,118 +82,79 @@ for msid in sorted(MS_Aliases_Dict.keys()):
 
                         if(structure not in Structs[struct_type][struct_stage]):
                             Structs[struct_type][struct_stage][structure]=dict()
-
                         Structs[struct_type][struct_stage][structure][external_id]=source
+
+                        if(struct_type in Formulas_Dict[source]):
+
+                            #There's a chance that extracting formula failed, see CHLOROPHYLLIDE-A
+                            if(external_id not in Formulas_Dict[source][struct_type][struct_stage]):
+                                continue
+
+                            formula = Formulas_Dict[source][struct_type][struct_stage][external_id]
+                            if(formula not in Formulas[struct_type][struct_stage]):
+                                Formulas[struct_type][struct_stage][formula]=dict()
+                            Formulas[struct_type][struct_stage][formula][external_id]=source
 
     if(len(Structs.keys())==0):
         continue
-                        
-    ms_structure = "null"
-    ms_structure_type = "null"
-    ms_external_ids = list()
 
-    if("SMILE" in Structs):
-        if("Charged" in Structs["SMILE"]):
-            if(len(Structs["SMILE"]["Charged"])==1):
-                structure = list(Structs["SMILE"]["Charged"].keys())[0]
-                ms_structure = structure
-                ms_structure_type = "SMILE"
-                ms_external_ids = Structs["SMILE"]["Charged"][structure].keys()
-            else:
-                for structure in Structs["SMILE"]["Charged"]:
-                    for external_id in Structs["SMILE"]["Charged"][structure]:
-                        smile_conflicts_file.write(msid+"\tCharged\t"+external_id+"\t"+structure+"\t"+Structs["SMILE"]["Charged"][structure][external_id]+"\n")
-                pass
+    #Charged InChI
+    #Single Formula? Remember!
+    #Check to see if, for one formula, there are multiple charges
+    #If single formula, then print all InChI, InChIKey, SMILE (select SMILE with same formula? select SMILE from KEGG?)
+    #If not single formula, print conflict
+    #If not Charged InChI
+    #Check Original InChI (repeat)
+    #Check Original SMILE (repeat)
 
-        elif("Original" in Structs["SMILE"]):
-            if(len(Structs["SMILE"]["Original"])==1):
-                structure = list(Structs["SMILE"]["Original"].keys())[0]
-                ms_structure = structure
-                ms_structure_type = "SMILE"
-                ms_external_ids = Structs["SMILE"]["Original"][structure].keys()
-            else:
-                #Establish rules for checking/curating SMILE strings
-                for structure in Structs["SMILE"]["Original"]:
-                    for external_id in Structs["SMILE"]["Original"][structure]:
-                        smile_conflicts_file.write(msid+"\tOriginal\t"+external_id+"\t"+structure+"\t"+Structs["SMILE"]["Original"][structure][external_id]+"\n")
-                pass
-    else:
-        pass
-
-    if(ms_structure != "null"):
-        unique_structs_file.write("\t".join([msid,ms_structure_type,";".join(sorted(ms_external_ids)),ms_structure])+"\n")
-    else:
-        #There are possible problems that arise where we can't find a unique SMILE structure, but we can find a unique InChI structure
-        #I don't yet know why, but I've a feeling it's to do with how some valences are assigned within a MOL file which
-        #is normalized using InChI as a standard, but breaks down when generating SMILE strings
-        #A good example is Protoheme: cpd00028
-        pass
-
-    ms_structure="null"
-    if("InChIKey" in Structs):
-
-        #Default to structures charged by MarvinBeans
-        if("Charged" in Structs["InChIKey"]):
-
-            if(len(Structs["InChIKey"]["Charged"])==1):
-                structure = list(Structs["InChIKey"]["Charged"].keys())[0]
-                ms_structure = structure
-                ms_structure_type = "InChIKey"
-                ms_external_ids = Structs["InChIKey"]["Charged"][structure].keys()
-            else:
-                #Establish rules for checking/curating InChIKey strings
-                pass
-
-        elif("Original" in Structs["InChIKey"]):
-            if(len(Structs["InChIKey"]["Original"])==1):
-                structure = list(Structs["InChIKey"]["Original"].keys())[0]
-                ms_structure = structure
-                ms_structure_type = "InChIKey"
-                ms_external_ids = Structs["InChIKey"]["Original"][structure].keys()
-            else:
-                #Establish rules for checking/curating InChIKey strings
-                pass
-
-    if(ms_structure != "null"):
-        unique_structs_file.write("\t".join([msid,ms_structure_type,";".join(sorted(ms_external_ids)),ms_structure])+"\n")
-
-    ms_structure="null"
+    struct_type=None
+    struct_stage=None
     if("InChI" in Structs):
+        struct_type="InChI"
+        if("Charged" in Structs[struct_type]):
+            struct_stage="Charged"
+        elif("Original" in Structs[struct_type]):
+            struct_stage="Original"
+    elif("SMILE" in Structs):
+        struct_type="SMILE"
+        if("Charged" in Structs[struct_type]):
+            struct_stage="Charged"
+        elif("Original" in Structs[struct_type]):
+            struct_stage="Original"
 
-        #Default to structures charged by MarvinBeans
-        if("Charged" in Structs["InChI"]):
+    if(struct_type is None or struct_stage is None):
+        #At time of writing, this doesn't happen
+        continue
 
-            if(len(Structs["InChI"]["Charged"])==1):
-                structure = list(Structs["InChI"]["Charged"].keys())[0]
-                ms_structure = structure
-                ms_structure_type = "InChI"
-                ms_external_ids = Structs["InChI"]["Charged"][structure].keys()
-            elif(len(Structs["InChI"]["Charged"])>1):
-#                structures = list(Structs["InChI"]["Charged"].keys())
-#                result = DifferObj.compare(structures[0], structures[1])
-#                sys.stdout.writelines(result)
+    struct_pass=0
+    struct_conflict=0
+    formula_conflict=0
+    if(len(Structs[struct_type][struct_stage].keys())==1):
+        struct_pass=1
+    elif(len(Formulas[struct_type][struct_stage].keys())==1):
+        struct_conflict=1
+        struct_pass=1
+    else:
+        struct_conflict=1
+        formula_conflict=1
+        pass
 
-                for structure in Structs["InChI"]["Charged"]:
-                    for external_id in Structs["InChI"]["Charged"][structure]:
-                        inchi_conflicts_file.write(msid+"\tCharged\t"+external_id+"\t"+structure+"\t"+Structs["InChI"]["Charged"][structure][external_id]+"\n")
-                pass
+    if(struct_pass):
+        #In order to replicate, print SMILE, InChIKey, InChI in order
+        for structure_type in "SMILE","InChIKey","InChI":
+            if(structure_type not in Structs):
+                continue
+            for structure in sorted(Structs[structure_type][struct_stage].keys()):
+                unique_structs_file.write("\t".join((msid,structure_type,";".join(sorted(Structs[structure_type][struct_stage][structure])),structure))+"\n")
 
-        elif("Original" in Structs["InChI"]):
-            if(len(Structs["InChI"]["Original"])==1):
-                structure = list(Structs["InChI"]["Original"].keys())[0]
-                ms_structure = structure
-                ms_structure_type = "InChI"
-                ms_external_ids = Structs["InChI"]["Original"][structure].keys()
-            else:
-                for structure in Structs["InChI"]["Original"]:
-                    for external_id in Structs["InChI"]["Original"][structure]:
-                        inchi_conflicts_file.write(msid+"\tOriginal\t"+external_id+"\t"+structure+"\t"+Structs["InChI"]["Original"][structure][external_id]+"\n")
-                pass
+    if(struct_conflict==1):
+        for structure in Structs[struct_type][struct_stage]:
+            for external_id in Structs[struct_type][struct_stage][structure]:
+                structure_conflicts_file.write("\t".join((msid,struct_type,struct_stage,structure,external_id,
+                                                          Structs[struct_type][struct_stage][structure][external_id]))+"\n")
 
-    if(ms_structure != "null"):
-        unique_structs_file.write("\t".join([msid,ms_structure_type,";".join(sorted(ms_external_ids)),ms_structure])+"\n")
-
-master_structs_file.close()
-unique_structs_file.close()
-inchi_conflicts_file.close()
-smile_conflicts_file.close()
+    if(formula_conflict==1):
+        for formula in Formulas[struct_type][struct_stage]:
+            for external_id in Formulas[struct_type][struct_stage][formula]:
+                formula_conflicts_file.write("\t".join((msid,struct_type,struct_stage,formula,external_id,
+                                                          Formulas[struct_type][struct_stage][formula][external_id]))+"\n")
