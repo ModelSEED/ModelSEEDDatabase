e5a633b7821cff9c09342a4920afa7d3d8e9e29b
seaver <seaver@mcs.anl.gov>
2019-02-13 14:25:53 -0600

    Updating electron charge and incorporating suggestions

--- Scripts/Biochemistry/Add_New_Compounds.py
+++ Scripts/Biochemistry/Add_New_Compounds.py
@@ -3,95 +3,95 @@ import os, sys, re, copy
 from csv import DictReader
 from collections import OrderedDict
 temp=list();
-header=1;
+header=True;
 
 sys.path.append('../../Libs/Python')
 from BiochemPy import Reactions, Compounds, InChIs
 
-CompoundsHelper = Compounds()
-Compounds_Dict = CompoundsHelper.loadCompounds()
+compounds_helper = Compounds()
+compounds_dict = compounds_helper.loadCompounds()
 
-Names_Dict = CompoundsHelper.loadNames()
-SearchNames_Dict = dict()
-All_Names = dict()
-New_Name_Count=dict()
-for msid in sorted(Names_Dict.keys()):
-    for name in Names_Dict[msid]:
-        All_Names[name]=1
+names_dict = compounds_helper.loadNames()
+searchnames_dict = dict()
+all_names_dict = dict()
+new_name_count = dict()
+for msid in sorted(names_dict):
+    for name in names_dict[msid]:
+        all_names_dict[name]=1
 
-        searchname = CompoundsHelper.searchname(name)
+        searchname = compounds_helper.searchname(name)
         #Avoid redundancy where possible
-        if(searchname not in SearchNames_Dict):
-            SearchNames_Dict[searchname]=msid
+        if(searchname not in searchnames_dict):
+            searchnames_dict[searchname]=msid
 
-Original_Alias_Dict=CompoundsHelper.loadMSAliases()
-Source_Alias_Dict = dict()
-All_Aliases=dict()
-New_Alias_Count=dict()
-for msid in Original_Alias_Dict.keys():
-    for source in Original_Alias_Dict[msid].keys():
-        if(source not in Source_Alias_Dict):
-            Source_Alias_Dict[source]=dict()
+original_alias_dict=compounds_helper.loadMSAliases()
+source_alias_dict = dict()
+all_aliases = dict()
+new_alias_count = dict()
+for msid in original_alias_dict:
+    for source in original_alias_dict[msid]:
+        if(source not in source_alias_dict):
+            source_alias_dict[source]=dict()
 
-        for alias in Original_Alias_Dict[msid][source]:
-            if(alias not in All_Aliases):
-                All_Aliases[alias]=dict()
-            All_Aliases[alias][msid]=1
+        for alias in original_alias_dict[msid][source]:
+            if(alias not in all_aliases):
+                all_aliases[alias]=dict()
+            all_aliases[alias][msid]=1
 
-            if(alias not in Source_Alias_Dict[source]):
-                Source_Alias_Dict[source][alias]=list()
-            Source_Alias_Dict[source][alias].append(msid)
+            if(alias not in source_alias_dict[source]):
+                source_alias_dict[source][alias]=list()
+            source_alias_dict[source][alias].append(msid)
 
-for alias in All_Aliases.keys():
-    All_Aliases[alias]=sorted(All_Aliases[alias].keys())
+for alias in all_aliases:
+    all_aliases[alias]=sorted(all_aliases[alias])
 
-Structures_Dict = CompoundsHelper.loadStructures(["InChI","SMILE"],["KEGG","MetaCyc"])
-All_InChIs=dict()
-All_Aliases_InChIs=dict()
-for alias in Structures_Dict['InChI'].keys():
+Structures_Dict = compounds_helper.loadStructures(["InChI","SMILE"],["KEGG","MetaCyc"])
+all_inchis=dict()
+all_aliases_InChIs=dict()
+for alias in Structures_Dict['InChI']:
     if('Charged' in Structures_Dict['InChI'][alias]):
-        for struct in Structures_Dict['InChI'][alias]['Charged'].keys():
-            if(struct not in All_InChIs):
-                All_InChIs[struct]=list()
-            All_InChIs[struct].append(alias)
+        for struct in Structures_Dict['InChI'][alias]['Charged']:
+            if(struct not in all_inchis):
+                all_inchis[struct]=list()
+            all_inchis[struct].append(alias)
 
-            if(alias not in All_Aliases_InChIs):
-                All_Aliases_InChIs[alias]=list()
-            All_Aliases_InChIs[alias].append(struct)
+            if(alias not in all_aliases_InChIs):
+                all_aliases_InChIs[alias]=list()
+            all_aliases_InChIs[alias].append(struct)
     elif('Original' in Structures_Dict['InChI'][alias]):
-        for struct in Structures_Dict['InChI'][alias]['Original'].keys():
-            if(struct not in All_InChIs):
-                All_InChIs[struct]=list()
-            All_InChIs[struct].append(alias)
+        for struct in Structures_Dict['InChI'][alias]['Original']:
+            if(struct not in all_inchis):
+                all_inchis[struct]=list()
+            all_inchis[struct].append(alias)
 
-            if(alias not in All_Aliases_InChIs):
-                All_Aliases_InChIs[alias]=list()
-            All_Aliases_InChIs[alias].append(struct)
+            if(alias not in all_aliases_InChIs):
+                all_aliases_InChIs[alias]=list()
+            all_aliases_InChIs[alias].append(struct)
 
-All_SMILEs=dict()
-All_Aliases_SMILEs=dict()
-for alias in Structures_Dict['SMILE'].keys():
+all_smiles=dict()
+all_aliases_SMILEs=dict()
+for alias in Structures_Dict['SMILE']:
     if('Charged' in Structures_Dict['SMILE'][alias]):
-        for struct in Structures_Dict['SMILE'][alias]['Charged'].keys():
-            if(struct not in All_SMILEs):
-                All_SMILEs[struct]=list()
-            All_SMILEs[struct].append(alias)
+        for struct in Structures_Dict['SMILE'][alias]['Charged']:
+            if(struct not in all_smiles):
+                all_smiles[struct]=list()
+            all_smiles[struct].append(alias)
 
-            if(alias not in All_Aliases_SMILEs):
-                All_Aliases_SMILEs[alias]=list()
-            All_Aliases_SMILEs[alias].append(struct)
+            if(alias not in all_aliases_SMILEs):
+                all_aliases_SMILEs[alias]=list()
+            all_aliases_SMILEs[alias].append(struct)
     elif('Original' in Structures_Dict['SMILE'][alias]):
-        for struct in Structures_Dict['SMILE'][alias]['Original'].keys():
-            if(struct not in All_SMILEs):
-                All_SMILEs[struct]=list()
-            All_SMILEs[struct].append(alias)
+        for struct in Structures_Dict['SMILE'][alias]['Original']:
+            if(struct not in all_smiles):
+                all_smiles[struct]=list()
+            all_smiles[struct].append(alias)
 
-            if(alias not in All_Aliases_SMILEs):
-                All_Aliases_SMILEs[alias]=list()
-            All_Aliases_SMILEs[alias].append(struct)
+            if(alias not in all_aliases_SMILEs):
+                all_aliases_SMILEs[alias]=list()
+            all_aliases_SMILEs[alias].append(struct)
 
 #Find last identifier and increment
-last_identifier = list(sorted(Compounds_Dict.keys()))[-1]
+last_identifier = list(sorted(compounds_dict))[-1]
 identifier_count = int(re.sub('^cpd','',last_identifier))
 
 Biochem="KEGG"
@@ -124,40 +124,40 @@ with open(Biochem_Root+Biochem+"_Compounds.tbl") as fh:
         (matched_cpd,matched_src)=(None,None)
 
         #First check that the Alias doesn't already exist
-        if(cpd['ID'] in Source_Alias_Dict[Biochem]):
-            matched_cpd = sorted(Source_Alias_Dict[Biochem][cpd['ID']])[0]
+        if(cpd['ID'] in source_alias_dict[Biochem]):
+            matched_cpd = sorted(source_alias_dict[Biochem][cpd['ID']])[0]
             matched_src="ID"
 
         #Then check that the Structure doesn't already exist, first as InChI, then as SMILE
-        if(matched_cpd is None and cpd['InChI'] and cpd['InChI'] in All_InChIs):
+        if(matched_cpd is None and cpd['InChI'] and cpd['InChI'] in all_inchis):
 
             msids = dict()
-            for alias in All_InChIs[cpd['InChI']]:
+            for alias in all_inchis[cpd['InChI']]:
 
                 #The structures are taken from their sources and the corresponding alias may not yet be registered
-                if(alias not in All_Aliases):
+                if(alias not in all_aliases):
                     continue
 
-                for msid in All_Aliases[alias]:
+                for msid in all_aliases[alias]:
                     msids[msid]=1
 
-            msids=list(sorted(msids.keys()))
+            msids=list(sorted(msids))
             if(len(msids)>0):
                 matched_cpd=msids[0]
                 matched_src='InChI'
 
-        if(matched_cpd is None and cpd['SMILE'] and cpd['SMILE'] in All_SMILEs):
+        if(matched_cpd is None and cpd['SMILE'] and cpd['SMILE'] in all_smiles):
 
             msids = dict()
-            for alias in All_SMILEs[cpd['SMILE']]:
+            for alias in all_smiles[cpd['SMILE']]:
                 #The structures are taken from their sources and the corresponding alias may not yet be registered
-                if(alias not in All_Aliases):
+                if(alias not in all_aliases):
                     continue
 
-                for msid in All_Aliases[alias]:
+                for msid in all_aliases[alias]:
                     msids[msid]=1
 
-            msids=list(sorted(msids.keys()))
+            msids=list(sorted(msids))
             if(len(msids)>0):
                 matched_cpd=msids[0]
                 matched_src='SMILE'
@@ -166,10 +166,10 @@ with open(Biochem_Root+Biochem+"_Compounds.tbl") as fh:
         if(matched_cpd is None):
             msids=dict()
             for name in cpd['NAMES'].split('|'):
-                searchname = CompoundsHelper.searchname(name)
-                if(searchname in SearchNames_Dict):
-                    msids[SearchNames_Dict[searchname]]=1
-            msids=list(sorted(msids.keys()))
+                searchname = compounds_helper.searchname(name)
+                if(searchname in searchnames_dict):
+                    msids[searchnames_dict[searchname]]=1
+            msids=list(sorted(msids))
             if(len(msids)>0):
                 matched_cpd=msids[0]
                 matched_src='NAMES'
@@ -184,33 +184,33 @@ with open(Biochem_Root+Biochem+"_Compounds.tbl") as fh:
             #NB at this point, names shouldn't match _anything_ already in the database
             #Names are saved separately as part of the aliases at the end of the script
             for name in cpd['NAMES'].split('|'):
-                if(name not in All_Names):
+                if(name not in all_names_dict):
                     #Possible for there to be no names in biochemistry?
-                    if(matched_cpd not in Names_Dict):
-                        Names_Dict[matched_cpd]=list()
-                    Names_Dict[matched_cpd].append(name)
-                    All_Names[name]=1
-                    New_Name_Count[matched_cpd]=1
+                    if(matched_cpd not in names_dict):
+                        names_dict[matched_cpd]=list()
+                    names_dict[matched_cpd].append(name)
+                    all_names_dict[name]=1
+                    new_name_count[matched_cpd]=1
 
             #print warning if multiple structures
-            if(cpd['InChI'] in All_InChIs):
-                if(cpd['ID'] not in All_Aliases_InChIs or cpd['ID'] not in All_InChIs[cpd['InChI']]):
-                    print("Warning: InChI structure for "+cpd['ID']+" assigned to different compounds: "+",".join(All_InChIs[cpd['InChI']]))
+            if(cpd['InChI'] in all_inchis):
+                if(cpd['ID'] not in all_aliases_InChIs or cpd['ID'] not in all_inchis[cpd['InChI']]):
+                    print("Warning: InChI structure for "+cpd['ID']+" assigned to different compounds: "+",".join(all_inchis[cpd['InChI']]))
 
             #print warning if multiple structures
-            if(cpd['SMILE'] in All_SMILEs):
-                if(cpd['ID'] not in All_Aliases_SMILEs or cpd['ID'] not in All_SMILEs[cpd['SMILE']]):
-                    print("Warning: SMILE structure for "+cpd['ID']+" assigned to different compounds: "+",".join(All_SMILEs[cpd['SMILE']]))
+            if(cpd['SMILE'] in all_smiles):
+                if(cpd['ID'] not in all_aliases_SMILEs or cpd['ID'] not in all_smiles[cpd['SMILE']]):
+                    print("Warning: SMILE structure for "+cpd['ID']+" assigned to different compounds: "+",".join(all_smiles[cpd['SMILE']]))
                 
             #if matching structure or name, add ID to aliases
             if(matched_src != 'ID'):
-                if(matched_cpd in Original_Alias_Dict and Biochem not in Original_Alias_Dict[matched_cpd]):
-                    Original_Alias_Dict[matched_cpd][Biochem]=list()
-                Original_Alias_Dict[matched_cpd][Biochem].append(cpd['ID'])
-                New_Alias_Count[matched_cpd]=1
+                if(matched_cpd in original_alias_dict and Biochem not in original_alias_dict[matched_cpd]):
+                    original_alias_dict[matched_cpd][Biochem]=list()
+                original_alias_dict[matched_cpd][Biochem].append(cpd['ID'])
+                new_alias_count[matched_cpd]=1
 
             #Update source type
-            Compounds_Dict[matched_cpd]['source']='Primary Database'
+            compounds_dict[matched_cpd]['source']='Primary Database'
 
         else:
 
@@ -226,8 +226,8 @@ with open(Biochem_Root+Biochem+"_Compounds.tbl") as fh:
             new_cpd['formula']=cpd['FORMULA']
 
             #Add new identifier with KEGG ID as alias
-            Original_Alias_Dict[new_cpd['id']]={Biochem:[cpd['ID']]}
-            New_Alias_Count[new_cpd['id']]=1
+            original_alias_dict[new_cpd['id']]={Biochem:[cpd['ID']]}
+            new_alias_count[new_cpd['id']]=1
 
             #Add new names
             #Names are saved separately as part of the aliases at the end of the script
@@ -236,13 +236,13 @@ with open(Biochem_Root+Biochem+"_Compounds.tbl") as fh:
                     new_cpd['name']=name
                     new_cpd['abbreviation']=name
 
-                if(name not in All_Names):
+                if(name not in all_names_dict):
                     #Possible for there to be no names in biochemistry?
-                    if(new_cpd['id'] not in Names_Dict):
-                        Names_Dict[new_cpd['id']]=list()
-                    Names_Dict[new_cpd['id']].append(name)
-                    All_Names[name]=1
-                    New_Name_Count[new_cpd['id']]=1
+                    if(new_cpd['id'] not in names_dict):
+                        names_dict[new_cpd['id']]=list()
+                    names_dict[new_cpd['id']].append(name)
+                    all_names_dict[name]=1
+                    new_name_count[new_cpd['id']]=1
 
             #If no names at all
             if(new_cpd['name']=='null'):
@@ -251,19 +251,19 @@ with open(Biochem_Root+Biochem+"_Compounds.tbl") as fh:
 
             #Add source type
             new_cpd['source']='Primary Database'
-            Compounds_Dict[new_cpd['id']]=new_cpd
+            compounds_dict[new_cpd['id']]=new_cpd
             New_Cpd_Count[new_cpd['id']]=1
 
 #Here, for matches, re-write names and aliases
 print("Compounds matched via:")
-for src in sorted(Matched_Cpd_Count.keys()):
+for src in sorted(Matched_Cpd_Count):
     print("\t"+src+": "+str(len(Matched_Cpd_Count[src])))
-print("Saving additional names for "+str(len(New_Name_Count))+" compounds")
-CompoundsHelper.saveNames(Names_Dict)
-print("Saving additional "+Biochem+" aliases for "+str(len(New_Alias_Count))+" compounds")
-CompoundsHelper.saveAliases(Original_Alias_Dict)
-print("Saving "+str(len(New_Cpd_Count.keys()))+" new compounds from "+Biochem)
-CompoundsHelper.saveCompounds(Compounds_Dict)
+print("Saving additional names for "+str(len(new_name_count))+" compounds")
+compounds_helper.saveNames(names_dict)
+print("Saving additional "+Biochem+" aliases for "+str(len(new_alias_count))+" compounds")
+compounds_helper.saveAliases(original_alias_dict)
+print("Saving "+str(len(New_Cpd_Count))+" new compounds from "+Biochem)
+compounds_helper.saveCompounds(compounds_dict)
 
 #Scripts to run afterwards
 #./Merge_Formulas.py
