b668e453bc9205a7ceb96f92bffbc50cd06a44a0
Sam Seaver <samseaver@gmail.com>
2019-04-11 16:21:56 -0500

    updating to snake-case dicts and checking for newly duplicated inchikeys

--- Scripts/Structures/Update_Compound_Structures_Formulas_Charge.py
+++ Scripts/Structures/Update_Compound_Structures_Formulas_Charge.py
@@ -23,45 +23,57 @@ with open(os.path.dirname(__file__)+'/ACPs_Master_Formula_Charge.txt') as fh:
                 continue
             Overridden_Fields[cpd][header[i]]=array[i]
 
-CompoundsHelper = Compounds()
-Structures_Dict = CompoundsHelper.loadStructures(["SMILE","InChI","InChIKey"],["ModelSEED"])
-Compounds_Dict = CompoundsHelper.loadCompounds()
+compounds_helper = Compounds()
+structures_dict = compounds_helper.loadStructures(["SMILE","InChI","InChIKey"],["ModelSEED"])
+compounds_dict = compounds_helper.loadCompounds()
+
+inchikey_dict = dict()
+for cpd in structures_dict:
+    if('InChIKey' in structures_dict[cpd]):
+        for struct in structures_dict[cpd]['InChIKey'].keys():
+            if(struct not in inchikey_dict):
+                inchikey_dict[struct]=list()
+            inchikey_dict[struct].append(cpd)
 
 Structures_Root=os.path.dirname(__file__)+"/../../Biochemistry/Structures/"
-for cpd in sorted (Compounds_Dict.keys()):
-    if(cpd not in Structures_Dict):
-        Compounds_Dict[cpd]['inchikey']=""
-        Compounds_Dict[cpd]['smiles']=""
+for cpd in sorted (compounds_dict.keys()):
+    if(cpd not in structures_dict):
+        compounds_dict[cpd]['inchikey']=""
+        compounds_dict[cpd]['smiles']=""
     else:
         formula_charge_dict={'formula':'null','charge':'0'}
         inchikey=""
         smile=""
-        if('InChI' in Structures_Dict[cpd]):
-            struct = list(Structures_Dict[cpd]['InChI'].keys())[0]
-            formula_charge_dict = Structures_Dict[cpd]['InChI'][struct]
-        elif('SMILE' in Structures_Dict[cpd]):
-            struct = list(Structures_Dict[cpd]['SMILE'].keys())[0]
-            formula_charge_dict = Structures_Dict[cpd]['SMILE'][struct]
+        if('InChI' in structures_dict[cpd]):
+            struct = list(structures_dict[cpd]['InChI'].keys())[0]
+            formula_charge_dict = structures_dict[cpd]['InChI'][struct]
+        elif('SMILE' in structures_dict[cpd]):
+            struct = list(structures_dict[cpd]['SMILE'].keys())[0]
+            formula_charge_dict = structures_dict[cpd]['SMILE'][struct]
         if(formula_charge_dict['charge']=='null'):
             formula_charge_dict['charge']='0'
 
-        if('InChIKey' in Structures_Dict[cpd]):
-            inchikey = sorted(list(Structures_Dict[cpd]['InChIKey'].keys()))[0]
-        if('SMILE' in Structures_Dict[cpd]):
-            smile = sorted(list(Structures_Dict[cpd]['SMILE'].keys()))[0]
+        if('InChIKey' in structures_dict[cpd]):
+            inchikey = sorted(list(structures_dict[cpd]['InChIKey'].keys()))[0]
+        if('SMILE' in structures_dict[cpd]):
+            smile = sorted(list(structures_dict[cpd]['SMILE'].keys()))[0]
 
-        Compounds_Dict[cpd]['inchikey']=inchikey
-        Compounds_Dict[cpd]['smiles']=smile
+        #See if any duplicates for newly assigned InChIKeys
+        if(compounds_dict[cpd]['inchikey']==""):
+            if(inchikey in inchikey_dict and len(inchikey_dict[inchikey])>1):
+                print("Warning: Duplicate InChIKey: "+inchikey+" in "+" and ".join(inchikey_dict[inchikey]))
+        compounds_dict[cpd]['inchikey']=inchikey
+        compounds_dict[cpd]['smiles']=smile
 
         if(formula_charge_dict['formula'] != "null"):
-            Compounds_Dict[cpd]['formula']=formula_charge_dict['formula']
-            Compounds_Dict[cpd]['charge']=int(formula_charge_dict['charge'])
+            compounds_dict[cpd]['formula']=formula_charge_dict['formula']
+            compounds_dict[cpd]['charge']=int(formula_charge_dict['charge'])
 
         #Override manually
         if(cpd in Overridden_Fields and 'formula' in Overridden_Fields[cpd]):
-            Compounds_Dict[cpd]['formula']=Overridden_Fields[cpd]['formula']
+            compounds_dict[cpd]['formula']=Overridden_Fields[cpd]['formula']
         if(cpd in Overridden_Fields and 'charge' in Overridden_Fields[cpd]):
-            Compounds_Dict[cpd]['charge']=int(Overridden_Fields[cpd]['charge'])
+            compounds_dict[cpd]['charge']=int(Overridden_Fields[cpd]['charge'])
 
 print("Saving compounds")
-CompoundsHelper.saveCompounds(Compounds_Dict)
+compounds_helper.saveCompounds(compounds_dict)
