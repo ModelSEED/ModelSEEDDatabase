37bea47fd17579d35f32ff56242a978aa65f938d
Sam Seaver <samseaver@gmail.com>
2019-08-22 14:53:58 -0500

    Extending intermediate parser to print out reaction table ready for integration

--- Integration/Yeast_8.3.4_2019/Parse_Yeast_8.3.4_Biochemistry.pl
+++ Integration/Yeast_8.3.4_2019/Parse_Yeast_8.3.4_Biochemistry.pl
@@ -53,32 +53,105 @@ foreach my $id (sort keys %Original_Compounds){
 }
 close(OUT);
 
-__END__
+my $Reactions = "yeastGEM_Reaction_Table.txt";
+open(FH, "< $Reactions");
+$header=1;
+my %Original_Reactions=();
+while(<FH>){
+    chomp;
+    if($header){$header--;next}
 
-open(OUT, "> ".$Output_Root."KEGG_Glycans.tbl");
-print OUT "ID\t".join("\t",@Headers)."\n";
-foreach my $id (sort keys %$Glycans){
-    print OUT $id,"\t";
-    foreach my $h (@Headers){
-	print OUT $Glycans->{$id}{$h};
-	print OUT "\t" unless $h eq $Headers[$#Headers];
-    }
-    print OUT "\n";
-}
-close(OUT);
+    #Convert ascii codes
+    $_ =~ s/__91__/[/g;
+    $_ =~ s/__93__/]/g;
 
-open(OUT, "> ".$Output_Root."KEGG_Reactions.tbl");
-@Headers = ("NAMES","KEGG","EQUATION","ECs");
-print OUT "ID\t".join("\t",@Headers)."\n";
-foreach my $c (sort { $a <=> $b } keys %$Reactions){
-    my @ids=sort grep { substr($_,0,1) eq "R" } split /\|/,$Reactions->{$c}{"KEGG"};
+    @temp=split(/\t/,$_);
 
-    print OUT $ids[0],"\t";
-    foreach my $h (@Headers){
-	if(!defined($Reactions->{$c}{$h})){
-	    print STDERR "Missing $h\n";
+    my $rxn = $temp[0];
+
+    #Clean up identifier
+    $rxn =~ s/^r_+//;
+
+    #Go through reactants
+    my ($rev,$reactants,$products)=@temp[2..5];
+
+    #Skipping all boundary exchange reactions
+    next if !$products;
+
+    my @reactants = split(/;/,$reactants);
+    my @eqn=();
+    my %cpts = (); #got to double-check
+    my $cpt_count = 0;
+    foreach my $rct (@reactants){
+	$rct =~ /s_(\d+)\[(\w+)\]\[(\d+(\.[\de-]+)*)\]/;
+	my ($cpd,$cpt,$coeff)=($1,$2,$3);
+
+	if(defined($coeff) && $coeff != 1){
+	    $coeff="(".$coeff.")";
+	}else{
+	    $coeff=undef($coeff);
 	}
-	print OUT $Reactions->{$c}{$h};
+
+	if(!exists($cpts{$cpt})){
+	    $cpts{$cpt}=$cpt_count;
+	    $cpt_count++;
+	}
+
+	$cpt = "[".$cpts{$cpt}."]";
+	
+	my $rct_str = $cpd.$cpt;
+	if(defined($coeff)){
+	    $rct_str = $coeff." ".$rct_str;
+	}
+
+	push(@eqn,$rct_str);
+    }
+
+    my $reversibility = "<=>";
+    if($rev ne "False"){
+	$reversibility = "=>";
+    }
+    push(@eqn,$reversibility);
+
+    my @products = split(/;/,$products);
+    foreach my $pdt (@products){
+	$pdt =~ /s_(\d+)\[(\w+)\]\[(\d+(\.[\de-]+)*)\]/;
+	my ($cpd,$cpt,$coeff)=($1,$2,$3);
+
+
+	if(defined($coeff) && $coeff != 1){
+	    $coeff="(".$coeff.")";
+	}else{
+	    $coeff=undef($coeff);
+	}
+
+	if(!exists($cpts{$cpt})){
+	    $cpts{$cpt}=$cpt_count;
+	    $cpt_count++;
+	}
+
+	$cpt = "[".$cpts{$cpt}."]";
+
+	my $pdt_str = $cpd.$cpt;
+	if(defined($coeff)){
+	    $pdt_str = $coeff." ".$pdt_str;
+	}
+	push(@eqn,$pdt_str);
+    }
+    my $eqn_str = join(" ",@eqn);
+
+    $Original_Reactions{$rxn}={'ID'=>$rxn,
+			       'NAMES'=>$temp[1],
+			       'EQUATION'=>$eqn_str};
+}
+close(FH);
+
+open(OUT, "> ".$filestub."_Reactions.tbl");
+@Headers=("ID","NAMES","EQUATION");
+print OUT join("\t",@Headers),"\n";
+foreach my $id (sort keys %Original_Reactions){
+    foreach my $h (@Headers){
+	print OUT $Original_Reactions{$id}{$h};
 	print OUT "\t" unless $h eq $Headers[$#Headers];
     }
     print OUT "\n";
