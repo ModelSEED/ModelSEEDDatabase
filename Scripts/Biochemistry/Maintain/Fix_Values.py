84339932164ecd6d006f16aa391b7a12d66ecc66
Sam Seaver <samseaver@gmail.com>
2020-04-13 13:25:16 -0500

    Making Fix_Values.py neutral

--- Scripts/Biochemistry/Maintain/Fix_Values.py
+++ Scripts/Biochemistry/Maintain/Fix_Values.py
@@ -4,27 +4,17 @@ from BiochemPy import Reactions, Compounds
 ##########################################
 # Fixing compounds
 ##########################################
-#compounds_helper = Compounds()
-#compounds_dict = compounds_helper.loadCompounds()
-#for cpd in compounds_dict:
-#    if(compounds_dict[cpd]['linked_compound']==""):
-#        compounds_dict[cpd]['linked_compound']=None
-compounds_helper.saveCompounds(compounds_dict)
+compounds_helper = Compounds()
+compounds_dict = compounds_helper.loadCompounds()
+for cpd in compounds_dict:
+    break
+#compounds_helper.saveCompounds(compounds_dict)
 
 ###############################################
 # Fixing reactions
 ###############################################
-#reactions_helper = Reactions()
-#reactions_dict = reactions_helper.loadReactions()
-
-#rxn = 'rxn42111'
-#notes = ['HB','GFP','EQP']
-#reactions_dict[rxn]['notes'] = notes
-
-#for rxn in reactions_dict:
-#    if(reactions_dict[rxn]['deltag'] == 10000000):
-#        reactions_dict[rxn]['deltag'] = 'null'
-#    if(reactions_dict[rxn]['deltagerr'] == 10000000):
-#        reactions_dict[rxn]['deltagerr'] = 'null'
-
+reactions_helper = Reactions()
+reactions_dict = reactions_helper.loadReactions()
+for rxn in reactions_dict:
+    break
 #reactions_helper.saveReactions(reactions_dict)
