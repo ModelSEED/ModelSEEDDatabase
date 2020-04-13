97d46dccca3bde1fca31a31279c6dd8a9f405192
Sam Seaver <samseaver@gmail.com>
2020-04-13 13:17:35 -0500

    Organizing scripts and adding draft README

---
+++ Scripts/Biochemistry/Maintain/Check_Links.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import os
+import sys
+import subprocess
+import time
+import copy
+import re
+import json
+from collections import OrderedDict
+from BiochemPy import Reactions, Compounds
+
+compounds_helper = Compounds()
+compounds_dict = compounds_helper.loadCompounds()
+cpds_aliases_dict = compounds_helper.loadMSAliases()
+cpds_names_dict = compounds_helper.loadNames()
+structures_dict = compounds_helper.loadStructures(["InChI","SMILE"],["ModelSEED"])
+
+for cpd in cpds_names_dict:
+    if(cpd not in compounds_dict):
+        print(cpd+" shouldn't be in names_dict")
+
+for cpd in cpds_aliases_dict:
+    if(cpd not in compounds_dict):
+        print(cpd+" shouldn't be in aliases_dict")
+
+for cpd in structures_dict:
+    if(cpd not in compounds_dict):
+        print(cpd+" shouldn't be in structures_dict")
+
+# Load Reactions
+reactions_helper = Reactions()
+reactions_dict = reactions_helper.loadReactions()
+rxns_aliases_dict = reactions_helper.loadMSAliases()
+rxns_names_dict = reactions_helper.loadNames()
+rxns_ecs_dict = reactions_helper.loadECs()
+
+for rxn in rxns_names_dict:
+    if(rxn not in reactions_dict):
+        print(rxn+" shouldn't be in names_dict")
+
+for rxn in rxns_aliases_dict:
+    if(rxn not in reactions_dict):
+        print(rxn+" shouldn't be in aliases_dict")
+
+for rxn in rxns_ecs_dict:
+    if(rxn not in reactions_dict):
+        print(rxn+" shouldn't be in ecs_dict")
---
+++ Scripts/Biochemistry/README.md
@@ -0,0 +1,33 @@
+Separated into Biochemistry refreshing, Biochemistry updating, and Biochemistry maintaining
+
+## The parent to check
+Reprint_Biochemistry.py
+
+## Updating 
+Add_New_Biochemistry_Attribute.py
+Remove_Biochemistry_Attribute.py
+Add_New_Compounds.py
+Add_New_Reactions.py
+Remove_Newly_Obsolescent_Compounds.py
+Remove_Newly_Obsolescent_Reactions.py
+
+## Refreshing
+Adjust_Reaction_Protons.py
+Adjust_Reaction_Water.py
+Merge_Formulas.py
+Merge_Obsolete_Aliases.py
+Merge_Reactions.py
+Rebalance_Reactions.py
+Rebuild_Reactions.py
+Update_Compound_Aliases.py
+Update_Reaction_Aliases.py
+Remove_Duplicate_Aliases.py
+
+## Maintaining
+Check_Charges.py
+Check_Formulas.py
+Check_Links.py
+Check_Template_Reactions.py
+Check_Transport.py
+Fix_Values.py
+Manual_Update_Links.py
