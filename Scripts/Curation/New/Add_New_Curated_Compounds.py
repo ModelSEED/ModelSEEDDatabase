5187c670f8b7a0ec280efd6177174fd16717550f
Sam Seaver <samseaver@gmail.com>
2022-07-12 11:25:09 -0500

    Reconfiguring reporting of matched compounds

--- Scripts/Curation/New/Add_New_Curated_Compounds.py
+++ Scripts/Curation/New/Add_New_Curated_Compounds.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('compounds_file', help="Compounds File")
 parser.add_argument('github_user', help="GitHub username")
 parser.add_argument("-s", dest='save_file', action='store_true')
+parser.add_argument("-r", dest='report_file', action='store_true')
 args = parser.parse_args()
 
 if(os.path.isfile(args.compounds_file) is False):
@@ -70,14 +71,21 @@ all_aliases_structures=dict()
 for structure_type in ["InChI","InChIKey","SMILE"]:
     for alias in Structures_Dict[structure_type]:
         if('Charged' in Structures_Dict[structure_type][alias]):
-            for struct in Structures_Dict[structure_type][alias]['Charged']:
-                if(struct not in all_structures):
-                    all_structures[struct]=list()
-                all_structures[struct].append(alias)
+            for structure in Structures_Dict[structure_type][alias]['Charged']:
+                struct_list = [structure]
+                if(structure_type == 'InChIKey'):
+                    struct_list.append('-'.join(structure.split('-')[0:2]))
+                    struct_list.append('-'.join(structure.split('-')[0:1]))
 
-                if(alias not in all_aliases_structures):
-                    all_aliases_structures[alias]=list()
-                all_aliases_structures[alias].append(struct)
+                for struct in struct_list:
+                    if(struct not in all_structures):
+                        all_structures[struct]=list()
+                    all_structures[struct].append(alias)
+
+                    if(alias not in all_aliases_structures):
+                        all_aliases_structures[alias]=list()
+                    all_aliases_structures[alias].append(struct)
+                    
         elif('Original' in Structures_Dict[structure_type][alias]):
             for struct in Structures_Dict[structure_type][alias]['Original']:
                 if(struct not in all_structures):
@@ -100,7 +108,7 @@ Default_Cpd = OrderedDict({ "id":"cpd00000","name":"null","abbreviation":"null",
                              "abstract_compound":"null","comprised_of":"null","linked_compound":"null",
                              "notes":[],"source":"" })
 New_Cpd_Count=dict()
-Matched_Cpd_Count=dict()
+matched_cpds_dict=dict()
 Headers=list()
 with open(args.compounds_file) as fh:
     for line in fh.readlines():
@@ -114,31 +122,40 @@ with open(args.compounds_file) as fh:
         for i in range(len(Headers)):
             cpd[Headers[i].lower()]=array[i]
 
-        (matched_cpd,matched_src)=(None,None)
+        (matched_cpd,matched_str,matched_src)=(None,None,None)
 
         #Check that the Structure doesn't already exist, first as InChI, then as InChiKey, then as SMILES
         if(matched_cpd is None):
             for structure_type in ['inchi','inchikey','smile','smiles']:
-                if(matched_cpd is not None):
-                    break
-                
-                if(structure_type in cpd and cpd[structure_type] in all_structures):
+                if(structure_type in cpd):
+                    structure_list = [cpd[structure_type]]
+                    if(structure_type == 'inchikey'):
+                        structure_list.append('-'.join(cpd[structure_type].split('-')[0:2]))
+                        structure_list.append('-'.join(cpd[structure_type].split('-')[0:1]))
+
+                    for structure in structure_list:
+                        
+                        if(matched_cpd is not None):
+                            break
+
+                        if(structure in all_structures):
                     
-                    msids = list()
-                    for alias in all_structures[cpd[structure_type]]:
+                            msids = list()
+                            for alias in all_structures[structure]:
 
-                        #The structures are taken from their sources and the corresponding alias may not yet be registered
-                        if(alias not in all_aliases):
-                            continue
+                                #The structures are taken from their sources and the corresponding alias may not yet be registered
+                                if(alias not in all_aliases):
+                                    continue
 
-                        for msid in all_aliases[alias]:
-                            if(msid not in msids):
-                                msids.append(msid)
+                                for msid in all_aliases[alias]:
+                                    if(msid not in msids):
+                                        msids.append(msid)
 
-                    msids=list(sorted(msids))        
-                    if(len(msids)>0):
-                        matched_cpd=msids[0]
-                        matched_src=structure_type
+                            msids=list(sorted(msids))
+                            if(len(msids)>0):
+                                matched_cpd=msids[0]
+                                matched_src=structure_type
+                                matched_str=structure
 
         #Then check that the Name doesn't already exist
         elif(matched_cpd is None):
@@ -146,16 +163,20 @@ with open(args.compounds_file) as fh:
             for name in cpd['names'].split('|'):
                 searchname = compounds_helper.searchname(name)
                 if(searchname in searchnames_dict):
-                    msids[searchnames_dict[searchname]]=1
-            msids=list(sorted(msids))
-            if(len(msids)>0):
-                matched_cpd=msids[0]
+                    if(searchnames_dict[searchname] not in msids):
+                        msids[searchnames_dict[searchname]]=name
+            msids_list=list(sorted(msids))
+            if(len(msids_list)>0):
+                matched_cpd=msids_list[0]
                 matched_src='NAMES'
+                matched_str=msids[msids_list[0]]
+
+        if(cpd['id'] not in matched_cpds_dict):
+            matched_cpds_dict[cpd['id']]=list()
+        match_dict = {'src':str(matched_src),'str':str(matched_str),'match':str(matched_cpd)}
+        matched_cpds_dict[cpd['id']].append(match_dict)
 
         if(matched_cpd is not None):
-            if(matched_src not in Matched_Cpd_Count):
-                Matched_Cpd_Count[matched_src]=dict()
-            Matched_Cpd_Count[matched_src][matched_cpd]=cpd['id']
 
             #Regardless of match-type, add new names
             #NB at this point, names shouldn't match _anything_ already in the database
@@ -176,7 +197,7 @@ with open(args.compounds_file) as fh:
 
             #print warning if multiple structures
             if('smile' in cpd and cpd['smile'] in all_structures):
-                if(cpd['ID'] not in all_aliases_structures or cpd['id'] not in all_structures[cpd['smile']]):
+                if(cpd['id'] not in all_aliases_structures or cpd['id'] not in all_structures[cpd['smile']]):
                     print("Warning: SMILE structure for "+cpd['id']+" assigned to different compounds: "+",".join(all_structures[cpd['smile']]))
                 
             #if matching structure or name, add ID to aliases
@@ -238,18 +259,37 @@ with open(args.compounds_file) as fh:
 
 #Here, for matches, re-write names and aliases
 print("Compounds matched via:")
-for src in sorted(Matched_Cpd_Count):
-    print("\t"+src+": "+str(len(Matched_Cpd_Count[src])))
-    #for match in Matched_Cpd_Count[src]:
-    #    print(src+"\t"+match+"\t"+Matched_Cpd_Count[src][match])
-print("Saving additional names for "+str(len(new_name_count))+" compounds")
+matched_src_dict=dict()
+for oc in sorted(matched_cpds_dict):
+    for match_dict in matched_cpds_dict[oc]:
+        if(match_dict['src'] not in matched_src_dict):
+            matched_src_dict[match_dict['src']]=list()
+        matched_src_dict[match_dict['src']].append(match_dict['match'])
+
+for src in matched_src_dict:
+    if(src != 'None'):
+        match_dict = dict()
+        for match in matched_src_dict[src]:
+            match_dict[match]=1
+        print("\t"+src+" "+str(len(matched_src_dict[src]))+" matched to "+str(len(match_dict.keys())))
+    else:
+        print("\t"+src+" "+str(len(matched_src_dict[src])))
+        
+if(args.report_file is True):
+    file_stub = '.'.join(args.compounds_file.split('.')[0:-1])
+    report_file = file_stub+'.rpt'
+    print("Saving report to file: "+report_file)
+    with open(report_file,'w') as rfh:
+        for oc in sorted(matched_cpds_dict):
+            for match_dict in matched_cpds_dict[oc]:
+                rfh.write(oc+'\t'+match_dict['match']+'\t'+match_dict['src']+'\t'+match_dict['str']+'\n')
+        
 if(args.save_file is True):
+    print("Saving additional names for "+str(len(new_name_count))+" compounds")
     compounds_helper.saveNames(names_dict)
-print("Saving additional "+curation_source+" aliases for "+str(len(new_alias_count))+" compounds")
-if(args.save_file is True):
+    print("Saving additional "+curation_source+" aliases for "+str(len(new_alias_count))+" compounds")
     compounds_helper.saveAliases(original_alias_dict)
-print("Saving "+str(len(New_Cpd_Count))+" new compounds from "+curation_source)
-if(args.save_file is True):
+    print("Saving "+str(len(New_Cpd_Count))+" new compounds from "+curation_source)
     compounds_helper.saveCompounds(compounds_dict)
 
 #Scripts to run afterwards
