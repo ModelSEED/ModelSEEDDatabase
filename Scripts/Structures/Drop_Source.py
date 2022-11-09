ed83f5889335626c997f7a99259af44724bb6b5c
seaver <samseaver@gmail.com>
2022-11-09 15:40:05 -0600

    Dropping ChEBI as source of structure again pending review of KEGG/MetaCyc structures

---
+++ Scripts/Structures/Drop_Source.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+Sources = ['ChEBI','Rhea']
+IDs_to_remove = list()
+All_lines = list()
+with open('../../Biochemistry/Structures/All_ModelSEED_Structures.txt') as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		if(tmp_list[4] not in Sources):
+			All_lines.append(line)
+		else:
+			IDs_to_remove.append(tmp_list[3])
+
+with open('../../Biochemistry/Structures/All_ModelSEED_Structures.txt','w') as fh:
+	fh.write('\n'.join(All_lines)+'\n')
+
+All_lines = list()
+with open('../../Biochemistry/Structures/Unique_ModelSEED_Structures.txt') as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		ids_list = tmp_list[2].split(';')
+		new_ids_list = list()
+		for id in ids_list:
+			if(id not in IDs_to_remove):
+				new_ids_list.append(id)
+		tmp_list[2] = ";".join(new_ids_list)
+		line = "\t".join(tmp_list)
+		All_lines.append(line)
+
+with open('../../Biochemistry/Structures/Unique_ModelSEED_Structures.txt', 'w') as fh:
+	fh.write('\n'.join(All_lines)+'\n')
