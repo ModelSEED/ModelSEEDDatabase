19a54536cb3f3066b83d52ca4dcc57c5304b2162
seaver <samseaver@gmail.com>
2022-12-01 13:39:12 -0600

    Adding ChEBI and Rhea data and scripts for first time

---
+++ Biochemistry/Aliases/Provenance/Rhea/Join_ChEBI_Names_Structures.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+##########################################
+# Load names
+ChEBI_names_dict = dict()
+
+with open('chebiId_name.tsv') as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		chebi_id = tmp_list[0].split(':')[1]
+		if(chebi_id not in ChEBI_names_dict):
+			ChEBI_names_dict[chebi_id]=list()
+		ChEBI_names_dict[chebi_id].append(tmp_list[1])
+
+with open('names.tsv') as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		if(tmp_list[1] not in ChEBI_names_dict):
+			ChEBI_names_dict[tmp_list[1]]=list()
+		if(tmp_list[4] not in ChEBI_names_dict[tmp_list[1]]):
+			ChEBI_names_dict[tmp_list[1]].append(tmp_list[4])
+
+##########################################
+# Load inchikey structures
+ChEBI_structures_dict = dict()
+
+ChEBI_File = "../../../Structures/ChEBI/InChIKey_ChargedStrings.txt"
+with open(ChEBI_File) as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		if(tmp_list[0] not in ChEBI_structures_dict):
+			ChEBI_structures_dict[tmp_list[0]]={'inchikey':'','smile':''}
+		ChEBI_structures_dict[tmp_list[0]]['inchikey']=tmp_list[1]
+
+ChEBI_File = "../../../Structures/ChEBI/InChIKey_OriginalStrings.txt"
+with open(ChEBI_File) as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		if(tmp_list[0] not in ChEBI_structures_dict):
+			ChEBI_structures_dict[tmp_list[0]]={'inchikey':'','smile':''}
+		
+		if(ChEBI_structures_dict[tmp_list[0]]['inchikey'] == ''):
+			ChEBI_structures_dict[tmp_list[0]]['inchikey']=tmp_list[1]
+
+##########################################
+# Load smile structures
+ChEBI_File = "../../../Structures/ChEBI/SMILE_ChargedStrings.txt"
+with open(ChEBI_File) as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		if(tmp_list[0] not in ChEBI_structures_dict):
+			ChEBI_structures_dict[tmp_list[0]]={'inchikey':'','smile':''}
+		ChEBI_structures_dict[tmp_list[0]]['smile']=tmp_list[1]
+
+ChEBI_File = "../../../Structures/ChEBI/SMILE_OriginalStrings.txt"
+with open(ChEBI_File) as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		if(tmp_list[0] not in ChEBI_structures_dict):
+			ChEBI_structures_dict[tmp_list[0]]={'inchikey':'','smile':''}
+		elif(ChEBI_structures_dict[tmp_list[0]] == ''):
+			ChEBI_structures_dict[tmp_list[0]]['smile']=tmp_list[1]
+
+TSV_File = "ChEBI_ID_Name_Structure.tsv"
+with open(TSV_File, 'w') as ofh:
+	ofh.write('\t'.join(['ID','Names','InChIKey','SMILES'])+'\n')
+	for chebi_id in sorted(ChEBI_names_dict):
+		line_list = [chebi_id]
+		line_list.append('|'.join(ChEBI_names_dict[chebi_id]))
+		if(chebi_id in ChEBI_structures_dict):
+			line_list.append(ChEBI_structures_dict[chebi_id]['inchikey'])
+			line_list.append(ChEBI_structures_dict[chebi_id]['smile'])
+		else:
+			line_list.append('')
+			line_list.append('')
+
+		line='\t'.join(line_list)
+		ofh.write(line+'\n')
---
+++ Biochemistry/Aliases/Provenance/Rhea/Join_Rhea_Names_Structures.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+##########################################
+# Load names
+Rhea_names_dict = dict()
+
+with open('Rhea_Generic_Polymer_Names.tsv') as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		if(tmp_list[0] not in Rhea_names_dict):
+			Rhea_names_dict[tmp_list[0]]=list()
+		Rhea_names_dict[tmp_list[0]].append(tmp_list[1])
+
+##########################################
+# Load inchikey structures
+Rhea_structures_dict = dict()
+
+Rhea_File = "../../../Structures/Rhea/InChIKey_ChargedStrings.txt"
+with open(Rhea_File) as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		rhea_id = ':'.join(tmp_list[0].split('_'))
+		if(rhea_id not in Rhea_structures_dict):
+			Rhea_structures_dict[rhea_id]={'inchikey':'','smile':''}
+		Rhea_structures_dict[rhea_id]['inchikey']=tmp_list[1]
+
+Rhea_File = "../../../Structures/Rhea/InChIKey_OriginalStrings.txt"
+with open(Rhea_File) as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		rhea_id = ':'.join(tmp_list[0].split('_'))
+		if(rhea_id not in Rhea_structures_dict):
+			Rhea_structures_dict[rhea_id]={'inchikey':'','smile':''}
+		
+		if(Rhea_structures_dict[rhea_id]['inchikey'] == ''):
+			Rhea_structures_dict[rhea_id]['inchikey']=tmp_list[1]
+
+##########################################
+# Load smile structures
+Rhea_File = "../../../Structures/Rhea/SMILE_ChargedStrings.txt"
+with open(Rhea_File) as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		rhea_id = ':'.join(tmp_list[0].split('_'))
+		if(rhea_id not in Rhea_structures_dict):
+			Rhea_structures_dict[rhea_id]={'inchikey':'','smile':''}
+		Rhea_structures_dict[rhea_id]['smile']=tmp_list[1]
+
+Rhea_File = "../../../Structures/Rhea/SMILE_OriginalStrings.txt"
+with open(Rhea_File) as fh:
+	for line in fh.readlines():
+		line = line.strip('\r\n')
+		tmp_list = line.split('\t')
+		rhea_id = ':'.join(tmp_list[0].split('_'))
+		if(rhea_id not in Rhea_structures_dict):
+			Rhea_structures_dict[rhea_id]={'inchikey':'','smile':''}
+		elif(Rhea_structures_dict[rhea_id] == ''):
+			Rhea_structures_dict[rhea_id]['smile']=tmp_list[1]
+
+TSV_File = "Rhea_ID_Name_Structure.tsv"
+with open(TSV_File, 'w') as ofh:
+	ofh.write('\t'.join(['ID','Names','InChIKey','SMILES'])+'\n')
+	for rhea_id in sorted(Rhea_names_dict):
+		line_list = [rhea_id]
+		line_list.append('|'.join(Rhea_names_dict[rhea_id]))
+		if(rhea_id in Rhea_structures_dict):
+			line_list.append(Rhea_structures_dict[rhea_id]['inchikey'])
+			line_list.append(Rhea_structures_dict[rhea_id]['smile'])
+		else:
+			line_list.append('')
+			line_list.append('')
+
+		line='\t'.join(line_list)
+		ofh.write(line+'\n')
