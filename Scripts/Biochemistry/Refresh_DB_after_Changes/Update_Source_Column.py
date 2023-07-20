04dae4664168cdf2941860ecd9e4f9f7ddcdf390
seaver <samseaver@gmail.com>
2023-07-20 16:13:25 -0500

    Updating the Source attribute of compounds to correctly register those that were curated

---
+++ Scripts/Biochemistry/Refresh_DB_after_Changes/Update_Source_Column.py
@@ -0,0 +1,61 @@
+import sys
+import os
+
+sys.path.append('../../../Libs/Python/')
+from BiochemPy import Reactions, Compounds
+
+biochem_root = os.path.dirname(__file__)+'/../../../Biochemistry/'
+curation_path = os.path.join(biochem_root,"Curation")
+curators_list=list()
+for dir in os.listdir(curation_path):
+	if(os.path.isdir(biochem_root+'Curation/'+dir)):
+		curators_list.append(dir)
+
+source_classes_dict = dict()
+with open('../../../Biochemistry/Aliases/Source_Classifiers.txt') as sc_fh:
+	for line in sc_fh.readlines():
+		line=line.strip('\r\n')
+		(source,classifier) = line.split('\t')
+		if(source in source_classes_dict):
+			print("Warning: source listed more than once")
+		source_classes_dict[source]=classifier
+
+compounds_helper = Compounds()
+compounds_dict = compounds_helper.loadCompounds()
+aliases_dict = compounds_helper.loadMSAliases()
+
+updated_compounds_list=list()
+for cpd in compounds_dict.keys():
+	
+	main_source_type = 'Orphan'
+
+	if(cpd not in aliases_dict):
+		#Set as Orphan
+		if(compounds_dict[cpd]['source']!='Orphan'):
+			compounds_dict[cpd]['source']='Orphan'
+			print("New Orphan Compound:",cpd)
+		continue
+
+	for source_type in 'Primary Database', 'Secondary Database', 'Published Model', 'User':
+		for source in aliases_dict[cpd]:
+			if(source not in source_classes_dict and source in curators_list):
+				main_source_type = "User"
+				break
+			if(source_classes_dict[source] == source_type):
+				main_source_type = source_type
+				break
+		if(main_source_type != 'Orphan'):
+				break
+		
+	if(compounds_dict[cpd]['source'].strip() == ''):
+		print("New Source for",cpd,":",main_source_type)
+		compounds_dict[cpd]['source']=main_source_type
+		updated_compounds_list.append(cpd)
+	elif(main_source_type != compounds_dict[cpd]['source']):
+		print("Change in Source for "+cpd+": From",compounds_dict[cpd]['source'],"to",main_source_type)
+		compounds_dict[cpd]['source']=main_source_type
+		updated_compounds_list.append(cpd)
+
+if(len(updated_compounds_list)>0):
+	print("Updating source for "+str(len(updated_compounds_list))+" compounds")
+	compounds_helper.saveCompounds(compounds_dict)
