4a29c66e9b9ec55e027fd9d0e6832fb6d05ada61
seaver <samseaver@gmail.com>
2023-07-21 16:17:20 -0500

    Updating the source of over a 1000 reactions to indicate their origins based on the alias. Several orphans reactions will be addressed directly

--- Scripts/Biochemistry/Refresh_DB_after_Changes/Update_Source_Column.py
+++ Scripts/Biochemistry/Refresh_DB_after_Changes/Update_Source_Column.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import os
 
@@ -59,3 +60,45 @@ for cpd in compounds_dict.keys():
 if(len(updated_compounds_list)>0):
 	print("Updating source for "+str(len(updated_compounds_list))+" compounds")
 	compounds_helper.saveCompounds(compounds_dict)
+
+reactions_helper = Reactions()
+reactions_dict = reactions_helper.loadReactions()
+aliases_dict = reactions_helper.loadMSAliases()
+
+updated_reactions_list=list()
+for rxn in reactions_dict.keys():
+	
+	main_source_type = 'Orphan'
+
+	if(rxn not in aliases_dict):
+		#Set as Orphan
+		if(reactions_dict[rxn]['source']!='Orphan'):
+			reactions_dict[rxn]['source']='Orphan'
+			print("New Orphan reaction:",rxn)
+		continue
+
+	for source_type in 'Primary Database', 'Secondary Database', 'Published Model', 'User':
+		for source in aliases_dict[rxn]:
+			if(source == 'rhea'):
+				continue
+			if(source not in source_classes_dict and source in curators_list):
+				main_source_type = "User"
+				break
+			if(source_classes_dict[source] == source_type):
+				main_source_type = source_type
+				break
+		if(main_source_type != 'Orphan'):
+				break
+		
+	if(reactions_dict[rxn]['source'].strip() == ''):
+		print("New Source for",rxn,":",main_source_type)
+		reactions_dict[rxn]['source']=main_source_type
+		updated_reactions_list.append(rxn)
+	elif(main_source_type != reactions_dict[rxn]['source']):
+		print("Change in Source for "+rxn+": From",reactions_dict[rxn]['source'],"to",main_source_type)
+		reactions_dict[rxn]['source']=main_source_type
+		updated_reactions_list.append(rxn)
+
+if(len(updated_reactions_list)>0):
+	print("Updating source for "+str(len(updated_reactions_list))+" reactions")
+	reactions_helper.saveReactions(reactions_dict)
