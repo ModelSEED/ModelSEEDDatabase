e572b7b888ec94753dc9d4eaca7444a5b73fdb02
seaver <samseaver@gmail.com>
2023-09-12 16:38:28 -0500

    Updating integration of thermodynamics in biochemistry for compounds

--- Scripts/Thermodynamics/Update_Compound_eQuilibrator_Energies.py
+++ Scripts/Thermodynamics/Update_Compound_eQuilibrator_Energies.py
@@ -24,8 +24,8 @@ with open(file_name) as file_handle:
         if('energy' in array[1] or array[1] == 'nan'):
             continue
 
-        eq_compounds[array[0]]={'dg':"{0:.2f}".format(float(array[1])),'dge':"{0:.2f}".format(float(array[2]))}
-
+        eq_compounds[array[0]]=[float("{0:.2f}".format(float(array[1]))),
+                                float("{0:.2f}".format(float(array[2])))]
 file_handle.close()
 
 # print(len(eq_compounds))
@@ -38,85 +38,63 @@ with open(file_name) as file_handle:
     for line in file_handle.readlines():
         line=line.strip()
         (mnx,inchikey)=line.split('\t')
-        
-        #This check makes sure that we use MetaNetX IDs for which we know we
-        #can retrieve energies
-        if(mnx not in eq_compounds):
-            continue
-
-        if('struct' not in eq_compounds[mnx]):
-            eq_compounds[mnx]['struct']=inchikey
-
-            #For searching purposes we lose the protonation indicator
-            inchikey="-".join(inchikey.split('-')[0:2])
-            struct_mnx_dict[inchikey]=mnx
+    
+        #For searching purposes we lose the protonation indicator
+        inchikey="-".join(inchikey.split('-')[0:2])
+        if(inchikey not in struct_mnx_dict):
+            struct_mnx_dict[inchikey]=list()
+        if(mnx not in struct_mnx_dict[inchikey]):
+            struct_mnx_dict[inchikey].append(mnx)
 
 file_handle.close()
 
 # print(len(struct_mnx_dict))
 # 18,206/19,432 (94%) MetaNetX records for which there is a unique structure
 
-seed_mnx_map=dict()
-for cpd in structures_dict:
-    structure_type='InChIKey'
-    if(structure_type not in structures_dict[cpd]):
-        continue
-
-    structure = list(structures_dict[cpd][structure_type].keys())[0]
-    dp_struct="-".join(structure.split('-')[0:2])
-
-    if(dp_struct not in struct_mnx_dict):
-        continue
-
-    seed_mnx_map[cpd]=eq_compounds[struct_mnx_dict[dp_struct]]
-
-# print(len(seed_mnx_map))
-# 17,863 ModelSEED compounds assigned eQuilibrator energies
-
-file_handle = open('Compounds_GroupContribution_eQuilibrator_Comparison.txt', 'w')
-file_handle.write('ID\tGC\tEQ\n')
 for cpd in sorted (compounds_dict.keys()):
 
-    cpd_gf='nan'
-    cpd_eq='nan'
+    #Default energy and error
+    dg_dge_list=[10000000.0,10000000.0]
 
-    if("GC" in compounds_dict[cpd]['notes']):
-        cpd_gf='|'.join([str(compounds_dict[cpd]['deltag']),str(compounds_dict[cpd]['deltagerr'])])
+    # Condition 1, no structure, use default
+    # Condition 2, structure is InChIKey or SMILE
 
-    if(cpd in seed_mnx_map):
-        cpd_eq='|'.join([seed_mnx_map[cpd]['dg'],seed_mnx_map[cpd]['dge']])
+    structure_type=None
+    if(cpd in structures_dict):
+        if('InChIKey' in structures_dict[cpd]):
+            structure_type = 'InChIKey' 
+        elif('SMILE' in structures_dict[cpd]):
+            structure_type = 'SMILE'
 
-    #Write the values to file. I'm not making exception, but I'm
-    #including this `if` statement as a comment to remind me of ways in which
-    #they can't be directly compared
-    #if(compounds_dict[cpd]['deltag']!=10000000 and cpd_gf != 'nan' and cpd_eq != 'nan'):
-    file_handle.write('\t'.join([cpd,cpd_gf,cpd_eq])+'\n')
+    structure = None
+    if(structure_type is not None):
+        structure = list(structures_dict[cpd][structure_type].keys())[0]
 
-    #Having printed to file, we skip where there is no eQuilibrator estimate
-    if(cpd not in seed_mnx_map):
-        continue
+    if(structure is not None):
 
-    notes_list=compounds_dict[cpd]['notes']
-    if(not isinstance(notes_list,list)):
-        notes_list=list()
+        deprotonated_struct="-".join(structure.split('-')[0:2])
 
-    #We indicate that an estimate is available
-    if('EQ' not in notes_list):
-        notes_list.append('EQ')
+        if(deprotonated_struct not in struct_mnx_dict):
+            continue
 
-    #Here we establish an arbitrary threshold of 50 for the error, if the error
-    #is too big, we don't use it
-    if(float(seed_mnx_map[cpd]['dge']) > 50):
-        continue
-
-    compounds_dict[cpd]['deltag']=float(seed_mnx_map[cpd]['dg'])
-    compounds_dict[cpd]['deltagerr']=float(seed_mnx_map[cpd]['dge'])
-
+        #In case where multiple energies because of distribution of bonds
+        #Take lowest energy as most likely result of equilibrium
+        #If the lowest energy is the default energy (i.e. 10000000)
+        #We will still save it
+        for mnx_id in struct_mnx_dict[deprotonated_struct]:
+            if(mnx_id not in eq_compounds):
+                continue
+            
+            if(eq_compounds[mnx_id][0] < dg_dge_list[0]):
+                dg_dge_list=eq_compounds[mnx_id]
+        
     #Here we indicate that we use the equilibrator value
-    if('EQU' not in notes_list):
-        notes_list.append('EQU')
-    compounds_dict[cpd]['notes']=notes_list
+    # values always saved as list of energy and error
+    if(compounds_dict[cpd]['thermodynamics'] == "null"):
+        compounds_dict[cpd]['thermodynamics'] = dict()
+    if('eQuilibrator' not in compounds_dict[cpd]['thermodynamics']):
+        compounds_dict[cpd]['thermodynamics']['eQuilibrator']=list()
+    compounds_dict[cpd]['thermodynamics']['eQuilibrator']=dg_dge_list
 
-file_handle.close()
 print("Saving compounds")
 compounds_helper.saveCompounds(compounds_dict)
