feca3da680788b476d1d718cb12d5cccefdd4bd3
Sam Seaver <seaver@anl.gov>
2024-01-09 14:31:04 -0600

    Updating how pKa is printed for each atom in a structure

--- Scripts/Structures/Compile_MB_Java.sh
+++ Scripts/Structures/Compile_MB_Java.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 /home/seaver/Software/Java_11/bin/javac -cp "/home/seaver/Software/MarvinBeans/lib/*:." -Xlint:deprecation $1
-cp $1 ../../../Structures/Mol/
+file=`basename $1 .java`
+cp ${file}.class ../../../Structures/Mol/
